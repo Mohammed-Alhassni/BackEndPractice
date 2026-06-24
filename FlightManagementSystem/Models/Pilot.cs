eb9083da81f59bfc34109ab3cfa947ec5a53dfc3
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2026-06-24 11:43:46 +0400

    Added all properities and comments for pilot model

--- FlightManagementSystem/Models/Pilot.cs
+++ FlightManagementSystem/Models/Pilot.cs
@@ -2,5 +2,10 @@ namespace FlightManagementSystem.Models;
 
 public class Pilot
 {
-    
+    public int PilotId  { get; set; } //System generated
+    public string PilotName { get; set; } //User input
+    public string PilotPhone { get; set; }  //User Input
+    public string LicenseNumber { get; set; } //User input
+    public int FlightHours  { get; set; } //User input 
+    public bool IsAvailable { get; set; } = true; //default value
 }
