7a239a620dd695d3ab74b95b0cf489a485bed2da
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2026-06-25 08:56:48 +0400

    Implemented db context for the models

--- FlightManagementSystem/Program.cs
+++ FlightManagementSystem/Program.cs
@@ -1,7 +1,18 @@
-﻿namespace FlightManagementSystem;
+﻿using FlightManagementSystem.Models;
+
+namespace FlightManagementSystem;
 
 class Program
 {
+    public static FMSContext DbContext = new FMSContext()
+    {
+        Aircrafts = new List<Aircraft>(),
+        Bookings = new List<Booking>(),
+        Flights = new List<Flight>(),
+        Passengers = new List<Passenger>(),
+        Pilots = new List<Pilot>()
+    };
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
