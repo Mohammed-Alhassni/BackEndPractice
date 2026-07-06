427ae345d5170bb65a3751721f7899ebc47400e7
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-07-06 18:21:21 +0400

    Implemented category attributes with source tages

--- ECommerceSystem/Models/Category.cs
+++ ECommerceSystem/Models/Category.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ECommerceSystem.Models
 {
     internal class Category
     {
+        public int CategoryId { get; set; } //system generated
+        public string CategoryName { get; set; } //user input 
+        public string Description { get; set; } //user input 
+        public string ImageUrl { get; set; } //user input 
+
     }
 }
