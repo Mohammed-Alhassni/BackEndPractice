ab387c5930170939fce3c255a4bb2df952bd392d
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-07-06 19:21:56 +0400

    Implemented attributes for Review model

--- ECommerceSystem/Models/Review.cs
+++ ECommerceSystem/Models/Review.cs
@@ -6,5 +6,11 @@ namespace ECommerceSystem.Models
 {
     internal class Review
     {
+        public int ReviewId { get; set; }
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+        public DateTime ReviewTime { get; set; }
     }
 }
