6906eb975b5c777d3b1eda412e3aa5acf4cad9c8
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-07-06 19:23:22 +0400

    Implemented all source tags for Review model

--- ECommerceSystem/Models/Review.cs
+++ ECommerceSystem/Models/Review.cs
@@ -6,11 +6,11 @@ namespace ECommerceSystem.Models
 {
     internal class Review
     {
-        public int ReviewId { get; set; }
-        public int UserId { get; set; }
-        public int ProductId { get; set; }
-        public int Rating { get; set; }
-        public string Comment { get; set; }
-        public DateTime ReviewTime { get; set; }
+        public int ReviewId { get; set; } //system generated 
+        public int UserId { get; set; } //from list
+        public int ProductId { get; set; } //from list
+        public int Rating { get; set; } //user input 
+        public string Comment { get; set; } //user input
+        public DateTime ReviewTime { get; set; } //system generated 
     }
 }
