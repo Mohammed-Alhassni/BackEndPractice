6cdb3cee20f8abb0cf42da0148f4b675c338901a
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-07-06 18:31:20 +0400

    Implemented Product model attributes

--- ECommerceSystem/Models/Product.cs
+++ ECommerceSystem/Models/Product.cs
@@ -6,5 +6,14 @@ namespace ECommerceSystem.Models
 {
     internal class Product
     {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsAvaiable { get; set; } = true;
     }
 }
