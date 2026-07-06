0959e613cb1a82512c117b34dd585937a49ed6ae
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-07-06 18:17:16 +0400

    Added source tags for User model attributes

--- ECommerceSystem/Models/User.cs
+++ ECommerceSystem/Models/User.cs
@@ -6,14 +6,14 @@ namespace ECommerceSystem.Models
 {
     internal class User
     {
-        public int UserId { get; set; } 
-        public string UserName { get; set; } 
-        public string Email { get; set; }
-        public string PasswordHash { get; set; } 
-        public string FullName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Address { get; set; }
-        public DateTime RegistrationDate { get; set; }
-        public bool isActive { get; set; } = true; 
+        public int UserId { get; set; } //system generated 
+        public string UserName { get; set; } //user input 
+        public string Email { get; set; } //user input 
+        public string PasswordHash { get; set; } //system calculated 
+        public string FullName { get; set; } //user input 
+        public string PhoneNumber { get; set; } //user input 
+        public string Address { get; set; } //user input 
+        public DateTime RegistrationDate { get; set; } //system generated 
+        public bool isActive { get; set; } = true;  //default value 
     }
 }
