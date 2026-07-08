f8538a2d962eb2e35d3fdd90440c317215128cfc
mohammed-alhassni <mohmmedmsoud@gmail.com>
2026-07-08 14:50:11 +0400

    Implemented in memory db context in Program

--- ECommerceSystem/Program.cs
+++ ECommerceSystem/Program.cs
@@ -1,7 +1,13 @@
-﻿namespace ECommerceSystem
+﻿
+
+namespace ECommerceSystem
 {
     public class Program
     {
+        //statruc in memory context, accesible by all
+        public static ECommerceContext DbContext = new ECommerceContext();
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
